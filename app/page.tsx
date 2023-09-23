f7e2fae0cce70e99d9d2313fe9226feb497e1887
Samy <s_rahim@estin.dz>
2023-09-23 07:29:55 +0100

    move to /predict

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen  p-24">
+    <main className=" h-screen  p-24">
       <div className="rounded-full absolute top-20 -left-40 w-80 h-80 bg-[#42057F] blur-3xl"></div>
       <div className="rounded-full absolute bottom-20 -right-40 w-80 h-80 bg-[#42057F] blur-3xl"></div>
-      {/* <input className="text-white bg-gradient-to-br from-[#ffffffbb] to-[#ffffffbb]"></input> */}
+      <h1 className="text-2xl">Sentiment Analyzer.ai</h1>
+      <textarea
+        className="w-full max-w-3xl text-white p-4 bg-[#42057F] border rounded-xl h-full max-h-[500px] "
+        placeholder="Place your text here ..."
+      ></textarea>
     </main>
   );
 }
